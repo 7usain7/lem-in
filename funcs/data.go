ce3808692efa1c6a500e77ac77fca27f7b1d5af2
Hussain <h.jameel2004@gmail.com>
2025-09-13 23:00:00 +0300

    creating rooms for start, end and normal rooms.

--- funcs/data.go
+++ funcs/data.go
@@ -1,15 +1,35 @@
 package funcs
 
 type Colony struct {
-	roomMap     map[string]*Room
-	antCount    int
-	roomPaths   map[*Room]int
-	workers     []*Ant
-	initialRoom *Room
-	finalRoom   *Room
+	rooms     map[string]*Room
+	antCount  int
+	roomPaths map[*Room]int
+	workers   []*Ant
+	startRoom *Room
+	endRoom   *Room
 }
 type Ant struct {
 }
 
 type Room struct {
+	tunnels       *TunnelList
+	isStart       bool
+	isDestination bool
+	isUnoccupied  bool
+	xCoord        int
+	yCoord        int
+	roomName      string
+	accessMap     map[string]bool
+	isEnd         bool
+}
+
+// tunnel between rooms
+type Tunnel struct {
+	data           *Room
+	nextConnection *Tunnel
+}
+
+// linked list of tunnels
+type TunnelList struct {
+	firstNode *Tunnel
 }
--- funcs/helpers.go
+++ funcs/helpers.go
@@ -69,5 +69,80 @@ func ParseInput(inputFile string, colony *Colony) (bool, string) {
 			hasProcessedFirstLine = true
 			continue
 		}
+		if isStart {
+			isStart = false
+			roomData := strings.Fields(currentLine)
+			if len(roomData) != 3 {
+				fmt.Printf("ERROR: invalid data format, invalid room definition (%s)\n", currentLine)
+				os.Exit(1)
+			}
+			roomName := roomData[0]
+			xPos, xErr := strconv.Atoi(roomData[1])
+			yPos, yErr := strconv.Atoi(roomData[2])
+			if xErr != nil || yErr != nil {
+				fmt.Printf("ERROR: invalid data format, invalid room definition (%s)\n", currentLine)
+				os.Exit(1)
+			}
+			colony.CreateRoom(roomName, "start", xPos, yPos)
+			continue
+		}
+
+		// Process end room specification
+		if isEnd {
+			isEnd = false
+			roomData := strings.Fields(currentLine)
+			if len(roomData) != 3 {
+				fmt.Printf("ERROR: invalid data format, invalid room definition (%s)\n", currentLine)
+				os.Exit(1)
+			}
+			roomName := roomData[0]
+			xPos, xErr := strconv.Atoi(roomData[1])
+			yPos, yErr := strconv.Atoi(roomData[2])
+			if xErr != nil || yErr != nil {
+				fmt.Printf("ERROR: invalid data format, invalid room definition (%s)\n", currentLine)
+				os.Exit(1)
+			}
+			colony.CreateRoom(roomName, "end", xPos, yPos)
+			continue
+		}
+	}
+}
+
+func (colony *Colony) CreateRoom(name string, roomType string, x int, y int) {
+	if _, exists := colony.rooms[name]; exists {
+		fmt.Printf("ERROR: invalid data format, duplicate room name '%s'\n", name)
+		os.Exit(1)
+	}
+
+	newRoom := &Room{
+		roomName:     name,
+		xCoord:       x,
+		yCoord:       y,
+		tunnels:      &TunnelList{},
+		accessMap:    make(map[string]bool),
+		isUnoccupied: true,
+	}
+
+	switch roomType {
+	case "start":
+		newRoom.isStart = true
+		newRoom.isEnd = false
+		colony.rooms[name] = newRoom
+		colony.startRoom = newRoom
+
+	case "end":
+		newRoom.isStart = false
+		newRoom.isEnd = true
+		colony.rooms[name] = newRoom
+		colony.endRoom = newRoom
+
+	case "normal":
+		newRoom.isStart = false
+		newRoom.isEnd = false
+		colony.rooms[name] = newRoom
+
+	default:
+		fmt.Printf("ERROR: invalid data format, invalid room type '%s'\n", roomType)
+		os.Exit(1)
 	}
 }
